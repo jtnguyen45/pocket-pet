46b9cba8801e0f0d782dfb7815ad96a1d91ce191
jtnguyen45 <jtnguyen45@gmail.com>
2024-03-14 14:44:10 -0700

    Add project skeleton and images, wip: HTML and CSS

---
+++ script.js
@@ -0,0 +1,19 @@
+/*----- constants -----*/
+const PET_STATES = {
+    HAPPY: "happy",
+    HUNGRY: "hungry",
+    SLEEPY: "sleepy",
+    DIRTY: "dirty",
+    BORED: "bored",
+}
+
+/*----- state variables -----*/
+
+
+/*----- cached elements  -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
