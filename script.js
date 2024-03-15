9683e6cde350488fd8ebe94dbffc4d3f25fa5f14
jtnguyen45 <jtnguyen45@gmail.com>
2024-03-15 14:30:14 -0700

    WIP: add basic functions for pet action and happiness/sad emotes

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const PET_STATES_GOOD = {
     "5": "having fun",
 }
 
-const PET_STATES_BAD = {
+const PET_STATES_BAD = { //could combine states and make bad neg num
     "1": "unhappy",
     "2": "hungry",
     "3": "sleepy",
@@ -24,23 +24,29 @@ const PET_DESC = {
 }
 
 /*----- state variables -----*/
+let petName = "Cinnamoroll";
 let happiness;
 let hunger;
 let energy;
 let clean;
 let fun;
 let level;
+let isPetAlive;
+let isActionHappening;
 
 /*----- cached elements  -----*/
 const messageEl = document.querySelector("h3");
+const resetBtn = document.querySelector("#resetBtn");
 const audio = document.getElementById("bgPlayer");
+const petImg = document.getElementById("overlayCinna");
 
 /*----- event listeners -----*/
-
+resetBtn.addEventListener("click", init);
+document.getElementById("action1").addEventListener("click", handleHappiness)
 
 /*----- functions -----*/
 init();
-audio.volume = 0.15;
+audio.volume = 0.10;
 
 function init() {
     happiness = 100;
@@ -49,15 +55,33 @@ function init() {
     clean = 100;
     fun = 100;
     level = 1;
+    isPetAlive = true;
+    isActionHappening = false;
+    startPetStatDec();
+    petImg.src = "/images/defaultCinna.gif";
 
     render();
 }
 
 function render() {
     //renderPet();
-    //renderMessage();
+    renderMessage();
+    renderControls();
     renderPetStats();
-    //renderControls();
+}
+
+function renderMessage() {
+    //change message about pet
+    if (happiness > 40) messageEl.innerHTML = `${petName} is ${PET_STATES_GOOD[1]} !!`;
+    if (happiness < 40) {
+        messageEl.innerHTML = `${petName} is ${PET_STATES_BAD[1]} !!`;
+        petImg.src = "/images/sadCinna.gif";
+    }
+}
+
+function renderControls() {
+    if (!isPetAlive) resetBtn.innerHTML = "PLAY AGAIN";
+    else resetBtn.innerHTML = "RESTART"; //add start -> restart -> play again
 }
 
 function renderPetStats() {
@@ -87,4 +111,25 @@ function getPetState(key) {
         default:
             return "";
     }
+}
+
+function startPetStatDec() {
+    setInterval(() => {
+        if (happiness !== 0 && !isActionHappening){
+            happiness -= 5;
+            render();
+        }
+    }, 3000);
+}
+
+function handleHappiness() {
+    isActionHappening = true;
+    happiness = happiness > 80 ? 100 : happiness += 15;
+    petImg.src = "/images/happyCinna.gif";
+    render();
+    setTimeout(() => {
+        isActionHappening = false;
+        petImg.src = "/images/defaultCinna.gif";
+        render();
+    }, 5000);
 }
