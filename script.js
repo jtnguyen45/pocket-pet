f55cd19a98ddfbb5b78e8dc52473d7c0ecee6568
jtnguyen45 <jtnguyen45@gmail.com>
2024-03-18 10:39:13 -0700

    Update handleAction function to support all 5 actions

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 /*----- constants -----*/
 const PET_STATES = {
-    "1": {good: "happy", bad: "unhappy", dec: 5, time: 4},
-    "2": {good: "full", bad: "hungry", dec: 10, time: 6},
-    "3": {good: "well-rested", bad: "sleepy", dec: 10, time: 10},
-    "4": {good: "clean", bad: "dirty", dec: 10, time: 8},
-    "5": {good: "having fun", bad: "bored", dec: 5, time: 5},
+    "1": {good: "happy", bad: "unhappy", value: 100, inc: 15, dec: 5, time: 4, img: "/images/happyCinna.gif"},
+    "2": {good: "full", bad: "hungry", value: 100, inc: 25, dec: 10, time: 6},
+    "3": {good: "well-rested", bad: "sleepy", value: 100, inc: 100, dec: 10, time: 10},
+    "4": {good: "clean", bad: "dirty", value: 100, inc: 100, dec: 10, time: 8},
+    "5": {good: "having fun", bad: "bored", value: 100, inc: 20, dec: 5, time: 5},
 }
 
 const PET_DESC = {
@@ -17,11 +17,6 @@ const PET_DESC = {
 
 /*----- state variables -----*/
 let petName = "Cinnamoroll";
-let happiness;
-let hunger;
-let energy;
-let clean;
-let fun;
 let level;
 let isPetAlive;
 let isActionHappening;
@@ -32,23 +27,25 @@ const messageEl = document.querySelector("h3");
 const resetBtn = document.querySelector("#resetBtn");
 const audio = document.getElementById("bgPlayer");
 const petImg = document.getElementById("overlayCinna");
-const bgImg = document.getElementById("petBackground");
 const images = document.getElementById("imageContainer")
 
 /*----- event listeners -----*/
 resetBtn.addEventListener("click", init);
-document.getElementById("action1").addEventListener("click", handleHappiness)
+document.getElementById("buttonsContainer").addEventListener("click", function(evt) {
+    const button = evt.target;
+    if (button.classList.contains("actionBtn")) handleAction(evt);
+});
 
 /*----- functions -----*/
 init();
 audio.volume = 0.10;
 
 function init() {
-    happiness = 100;
-    hunger = 100;
-    energy = 80;
-    clean = 100;
-    fun = 100;
+    PET_STATES[1].value = 100;
+    PET_STATES[2].value  = 100;
+    PET_STATES[3].value  = 80;
+    PET_STATES[4].value  = 100;
+    PET_STATES[5].value  = 100;
     level = 1;
     isPetAlive = true;
     isActionHappening = false;
@@ -73,14 +70,14 @@ function renderPet() {
         imgSrc = "/images/cryingCinna.gif";
         images.style.filter = "brightness(0.5)";
     } 
-    else if (happiness < 40 && !isActionHappening) imgSrc = "/images/sadCinna.gif";
+    else if (PET_STATES[1].value < 40 && !isActionHappening) imgSrc = "/images/sadCinna.gif";
     petImg.src = imgSrc;
 }
 
 function renderMessage() {
     //change message about pet
-    if (happiness > 40) messageEl.innerHTML = `${petName} is ${PET_STATES[1].good} !!`;
-    if (happiness < 40 && !isActionHappening) {
+    if (PET_STATES[1].value > 40) messageEl.innerHTML = `${petName} is ${PET_STATES[1].good} !!`;
+    if (PET_STATES[1].value < 40 && !isActionHappening) {
         messageEl.innerHTML = `${petName} is ${PET_STATES[1].bad} !!`;
     }
     if (!isPetAlive) messageEl.innerHTML = `You neglected ${petName}! They ran away from home!`;
@@ -95,29 +92,12 @@ function renderPetStats() {
     Object.keys(PET_DESC).forEach(key => {
         const cellId = `stat${key}`;
         const cellElement = document.getElementById(cellId);
-        cellElement.innerHTML = `${PET_DESC[key].toUpperCase()}: ${getPetState(parseInt(key))}`;
+        cellElement.innerHTML = `${PET_DESC[key].toUpperCase()}: ${PET_STATES[key].value}`;
     });
 
     const cellId = "petLevel";
     const cellElement = document.getElementById(cellId);
     cellElement.innerHTML = `Level: ${level}`;
-}
-
-function getPetState(key) {
-    switch (key) {
-        case 1:
-            return happiness;
-        case 2:
-            return hunger;
-        case 3:
-            return energy;
-        case 4:
-            return clean;
-        case 5:
-            return fun;
-        default:
-            return "";
-    }
 }
 
 function decPetStat() {
@@ -129,25 +109,7 @@ function decPetStat() {
         const sec = PET_STATES[key].time;
         const intervalId = setInterval(() => {
             if (!isActionHappening && isPetAlive) {
-                switch (parseInt(key)) {
-                    case 1:
-                        happiness -= dec;
-                        break;
-                    case 2:
-                        hunger -= dec;
-                        break;
-                    case 3:
-                        energy -= dec;
-                        break;
-                    case 4:
-                        clean -= dec;
-                        break;
-                    case 5:
-                        fun -= dec;
-                        break;
-                    default:
-                        break;
-                }
+                PET_STATES[key].value -= dec;
                 render();
             }
         }, sec * 1000);
@@ -156,18 +118,20 @@ function decPetStat() {
 }
 
 function checkPetAlive() {
-    if (happiness <= 0 || hunger <= 0 || energy <= 0 || clean <= 0 || fun <= 0) {
+    if (PET_STATES[1].value <= 0 || PET_STATES[2].value <= 0 || PET_STATES[3].value <= 0 || PET_STATES[4].value <= 0 || PET_STATES[5].value <= 0) {
         isPetAlive = false;
     }
 }
 
-function handleHappiness() {
+function handleAction(evt) {
     isActionHappening = true;
-    petImg.src = "/images/happyCinna.gif";
-    happiness = happiness > 80 ? 100 : happiness += 15;
-    //render();
+    const button = evt.target;
+    const actionId = button.id;
+
+    const key = parseInt(actionId.charAt(6));
+    PET_STATES[key].value = PET_STATES[key].value + PET_STATES[key].inc > 100 ? 100 : PET_STATES[key].value + PET_STATES[key].inc;
+    petImg.src = PET_STATES[key].img;
     setTimeout(() => {
-        petImg.src = happiness >= 40 ? "/images/defaultCinna.gif" : "images/sadCinna.gif";
         isActionHappening = false;
         render();
     }, 5000);
